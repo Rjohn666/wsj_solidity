e2afba6d5f8b757b7a7e2cbcb505891b8f749237
daddywu <1035049566@qq.com>
2023-05-14 05:49:31 +0800

    -

--- contract/just_test/test.sol
+++ contract/just_test/test.sol
@@ -1,24 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
-contract test {
-    uint256 master_rate = 185.7142857143 * 10 ** 12;
-    uint256 son_rate = 1.3 * 10 ** 3;
+contract Test {
+    uint256 public a;
+    uint256 public b;
 
-    function query_cross_need(
-        uint256 zm_amount
-    ) public view returns (uint256 master_amount, uint256 son_amount) {
-        // 判断是否能被130整除
-        if (zm_amount % 130000000000000000000 == 0) {
-            // 返回整数0.7
-            master_amount =
-                (zm_amount / 130000000000000000000) *
-                700000000000000000;
-        } else {
-            // 有余数，放大比例返回
-            master_amount = (zm_amount * 10 ** 12) / master_rate;
-        }
-        son_amount = (zm_amount * 10 ** 3) / son_rate;
+    constructor(uint256 _a, uint256 _b) {
+        a = _a;
+        b = _b;
+    }
+
+    function add() public view returns (uint256) {
+        return a + b;
     }
 }
